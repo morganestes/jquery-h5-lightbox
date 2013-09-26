4ce3f36ae80d851a02bffe37339dc09d09ecf256
Morgan Estes <morgan@golftailor.com>
2013-09-26 12:27:20 -0500

    Release v1.2.11

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,6 +86,6 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
 
   // Build for release.
-  grunt.registerTask('release', ['bump-only', 'default', 'bump-commit']);
+  grunt.registerTask('release', ['bump-only', 'clean', 'concat', 'uglify', 'bump-commit']);
 
 };
